079b214a96c3249018ecc991860cef2f0718fd6d
MontassarSakouhi <Montassa015@gmail.com>
2024-11-30 10:15:03 +0100

    first

---
+++ script.js
@@ -0,0 +1,27 @@
+document.querySelector('#btn').addEventListener('click',()=>{
+    const input=document.querySelector('#input').value
+    const weather=document.querySelector('#weather')
+    const temp=document.querySelector('#temp')
+    const clouds=document.querySelector('#clouds')
+    const humidity=document.querySelector('#humidity')
+    const name=document.querySelector('#name')
+    const api=`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=a7f6ddcbcdb7b8ea767e5cba77fb7fab&units=metric`
+   
+
+
+    fetch(api)
+    .then(response => response.json())
+    .then(data => {console.log(data)
+ 
+        name.innerHTML= `Name: ${data.name}`;
+        weather.innerHTML = `Weather: ${data.weather[0].description}`;
+        temp.innerHTML = `Temperature: ${data.main.temp} °C`;
+        clouds.innerHTML = `Clouds: ${data.clouds.all}%`;
+        humidity.innerHTML = `Humidity: ${data.main.humidity}%`
+
+    })
+    
+    // weather.innerHTML=
+    
+
+})
